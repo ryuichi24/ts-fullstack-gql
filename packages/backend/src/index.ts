fe442917335e058f0cb43247dfb1d0029e952a77
ryuichi24 <ryuichi.nishi24@gmail.com>
2023-01-06 15:20:25 +0900

    add cors

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,6 +4,7 @@ import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { expressMiddleware } from "@apollo/server/express4";
 import express from "express";
+import cors from 'cors'
 import { PrismaClient } from "@prisma/client";
 import { MyContext } from "./types/graphql.js";
 import { buildSchema } from "./utils/buildSchema.js";
@@ -23,6 +24,10 @@ async function main() {
   });
 
   await server.start();
+
+  app.use(cors({
+    origin: ['http://localhost:5173']
+  }))
 
   app.use(express.json());
 
