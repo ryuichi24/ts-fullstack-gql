ac1ce3c44907b59981a64e8e9d25b1281f3dc990
ryuichi24 <ryuichi.nishi24@gmail.com>
2023-01-12 23:27:39 +0900

    add Todo CURD logic in UI

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,116 @@
-import React from "react";
-import { useTodosQuery } from "./__generated__/graphql";
+import React, { useState } from "react";
+import {
+  TodosDocument,
+  useMakeTodoMutation,
+  useRemoveTodoMutation,
+  useTodosQuery,
+  useUpdateTodoMutation,
+} from "./__generated__/graphql";
 import { Layout } from "./components/Layout";
 import { TodoItem } from "./components/TodoItem";
 import { Button } from "./components/Elements/Button";
 import { InputField } from "./components/Elements/InputField";
 
 function App() {
+  const [title, setTitle] = useState<string>("");
   const { data, loading, error } = useTodosQuery();
+  const [makeTodoMut, { loading: makeTodoMutLoading }] = useMakeTodoMutation();
+  const [removeTodoMut, { loading: removeTodoMutLoading }] =
+    useRemoveTodoMutation();
+  const [updateTodoMut, { loading: updateTodoMutLoading }] =
+    useUpdateTodoMutation();
+
+  const handleTitleInputChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => setTitle(event.target.value);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+
+    try {
+      await makeTodoMut({
+        variables: {
+          makeTodoInput: {
+            title,
+          },
+        },
+        refetchQueries: [TodosDocument],
+      });
+
+      setTitle("");
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  };
+
+  const removeTodo = async (id: string) => {
+    try {
+      await removeTodoMut({
+        variables: {
+          removeTodoInput: {
+            todoId: id,
+          },
+        },
+        refetchQueries: [TodosDocument],
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  };
+
+  const updateTodoCompleteStatus = async (id: string, isCompleted: boolean) => {
+    try {
+      updateTodoMut({
+        variables: {
+          updateTodoInput: {
+            todoId: id,
+            isCompleted,
+          },
+        },
+        refetchQueries: [TodosDocument],
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  };
+
+  const updateTodoTitle = async (id: string, title: string) => {
+    try {
+      updateTodoMut({
+        variables: {
+          updateTodoInput: {
+            todoId: id,
+            title,
+          },
+        },
+        refetchQueries: [TodosDocument],
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  };
 
   return (
     <Layout>
       <div className="max-w-xl mx-auto p-7">
         <div className="bg-white p-6 rounded shadow">
-          <form className="flex flex-col">
+          <form className="flex flex-col" onSubmit={handleSubmit}>
             <InputField
               containerClassName="flex flex-col mb-6"
               labelName="New Todo"
               placeholder="buy some food..."
+              onChange={handleTitleInputChange}
+              value={title}
             />
             <Button size="sm" variant="primary">
               Add Todo
@@ -24,7 +118,13 @@ function App() {
           </form>
         </div>
         {data?.getTodos?.todos?.map((item) => (
-          <TodoItem key={item?.id} todoItem={item!} />
+          <TodoItem
+            key={item?.id}
+            todoItem={item!}
+            removeTodo={removeTodo}
+            updateTodoCompleteStatus={updateTodoCompleteStatus}
+            updateTodoTitle={updateTodoTitle}
+          />
         ))}
       </div>
     </Layout>
--- packages/frontend/src/components/TodoItem/TodoItem.tsx
+++ packages/frontend/src/components/TodoItem/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BiTrash } from "react-icons/bi";
 import { Todo } from "../../__generated__/graphql";
@@ -6,9 +6,58 @@ import { DropdownMenu } from "../Elements/DropdownMenu";
 
 type TodoItemProps = {
   todoItem: Todo;
+  removeTodo: (id: string) => void | Promise<void>;
+  updateTodoCompleteStatus: (
+    id: string,
+    isCompleted: boolean
+  ) => void | Promise<void>;
+  updateTodoTitle: (id: string, title: string) => void | Promise<void>;
 };
 
-export const TodoItem: React.FC<TodoItemProps> = ({ todoItem }) => {
+export const TodoItem: React.FC<TodoItemProps> = ({
+  todoItem,
+  removeTodo,
+  updateTodoCompleteStatus,
+  updateTodoTitle,
+}) => {
+  const [isTitleEditting, setIsTitleEditting] = useState<boolean>(false);
+  const [todoTitleInput, setTodoTitleInput] = useState<string>(todoItem.title);
+
+  const handleRemoveBtnClick: React.MouseEventHandler<HTMLButtonElement> = (
+    event
+  ) => {
+    removeTodo(todoItem.id);
+  };
+
+  const handleCompleteTodoCheckboxChange: React.ChangeEventHandler<
+    HTMLInputElement
+  > = (event) => {
+    updateTodoCompleteStatus(todoItem.id, event.target.checked);
+  };
+
+  const handleTodoTitleInput: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => setTodoTitleInput(event.target.value);
+
+  const handleEditTitleBtnClick: React.MouseEventHandler<HTMLButtonElement> = (
+    event
+  ) => {
+    setIsTitleEditting(true);
+  };
+
+  const handleTodoTitleInputBlur: React.FocusEventHandler<
+    HTMLInputElement
+  > = async (event) => {
+    try {
+      await updateTodoTitle(todoItem.id, todoTitleInput);
+      setIsTitleEditting(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  };
+
   return (
     <article
       className={`flex justify-between items-center shadow-sm bg-white rounded border-l-8 my-4 px-4 h-20 ${
@@ -21,17 +70,29 @@ export const TodoItem: React.FC<TodoItemProps> = ({ todoItem }) => {
           checked={todoItem?.isCompleted}
           type={"checkbox"}
           className="w-4 h-4"
+          onChange={handleCompleteTodoCheckboxChange}
         />
         <div className="flex flex-col ml-4">
-          <p
-            className={`${
-              todoItem?.isCompleted
-                ? "text-emerald-500 line-through"
-                : "text-slate-600"
-            }`}
-          >
-            {todoItem?.title}
-          </p>
+          {isTitleEditting ? (
+            <input
+              className="focus:outline-none text-slate-600"
+              type={"text"}
+              value={todoTitleInput}
+              onChange={handleTodoTitleInput}
+              autoFocus
+              onBlur={handleTodoTitleInputBlur}
+            />
+          ) : (
+            <p
+              className={`${
+                todoItem?.isCompleted
+                  ? "text-emerald-500 line-through"
+                  : "text-slate-600"
+              }`}
+            >
+              {todoItem?.title}
+            </p>
+          )}
 
           <small className="text-gray-400">
             {todoItem?.createdAt.toISOString().split("T")[0]}
@@ -46,11 +107,19 @@ export const TodoItem: React.FC<TodoItemProps> = ({ todoItem }) => {
           </small>
         }
         menuItems={[
-          <button key={1} className="flex justify-between w-full items-center text-gray-800 cursor-pointer">
+          <button
+            onClick={handleEditTitleBtnClick}
+            key={1}
+            className="flex justify-between w-full items-center text-gray-800 cursor-pointer"
+          >
             <span>Edit</span>
             <AiOutlineEdit />
           </button>,
-          <button key={2} className="flex justify-between w-full items-center text-red-400 cursor-pointer">
+          <button
+            onClick={handleRemoveBtnClick}
+            key={2}
+            className="flex justify-between w-full items-center text-red-400 cursor-pointer"
+          >
             <span>Remove</span>
             <BiTrash />
           </button>,
